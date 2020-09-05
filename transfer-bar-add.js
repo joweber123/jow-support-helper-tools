80a1b6258029861f9e9e868adc5e6cdbe8101fc8
James <joweber123@gmail.com>
2020-09-05 21:33:35 +0900

    Update transfer-bar-add.js
    Editing version number.

--- transfer-bar-add.js
+++ transfer-bar-add.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Transfer Bar Add
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Extends Happychat Operators Script to add Operators
 // @author       joweber123
 // @match        https://hud.happychat.io/*
