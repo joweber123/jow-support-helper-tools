971e5315bc644badda4fce33453941c74830cf7e
James <joweber123@gmail.com>
2020-09-08 15:10:16 +0900

    Update transfer-bar-add.js
    Changing let variables to const

--- transfer-bar-add.js
+++ transfer-bar-add.js
@@ -1,4 +1,4 @@
-// ==UserScript==
+ // ==UserScript==
 // @name         Transfer Bar Add
 // @namespace    http://tampermonkey.net/
 // @version      1.1
@@ -10,44 +10,43 @@
 
 //Calculate the load of all HEs with status as transfer
 function transferLoadCalculator(){
-    let transferLoadNumArray = [];
+    const transferLoadNumArray = [];
     document.querySelectorAll('.operators__busy.hasName').forEach((transferHEElement)=>{
-        let transferInfoRaw = transferHEElement.firstElementChild.getAttribute('title');
-        let transferLoadPos = transferInfoRaw.indexOf('Load: ');
-        let transferLoadNum = transferInfoRaw.substr(transferLoadPos+6, 1);
+        const transferInfoRaw = transferHEElement.firstElementChild.getAttribute('title');
+        const transferLoadPos = transferInfoRaw.indexOf('Load: ');
+        const transferLoadNum = transferInfoRaw.substr(transferLoadPos+6, 1);
         transferLoadNumArray.push(parseInt(transferLoadNum,10))
     })
-    let transferLoadFinal = transferLoadNumArray.reduce((a,b) => a+b, 0)
+    const transferLoadFinal = transferLoadNumArray.reduce((a,b) => a+b, 0)
     return transferLoadFinal;
 }
 
 //Calculate the total throttle capacity of all HEs with status as transfer
 function transferThrottleCalculator(){
-    let transferThrottleArray =[];
+    const transferThrottleArray =[];
     document.querySelectorAll('.operators__busy.hasName').forEach((transferHEElement)=>{
-        let transferInfoRaw = transferHEElement.firstElementChild.getAttribute('title');
-        let transferThrottlePos = transferInfoRaw.indexOf('Throttle: ');
-        let transferThrottleNum = transferInfoRaw.substr(transferThrottlePos+10, 1);
+        const transferInfoRaw = transferHEElement.firstElementChild.getAttribute('title');
+        const transferThrottlePos = transferInfoRaw.indexOf('Throttle: ');
+        const transferThrottleNum = transferInfoRaw.substr(transferThrottlePos+10, 1);
         transferThrottleArray.push( parseInt(transferThrottleNum,10));
     })
-    let transferThrottleFinal = transferThrottleArray.reduce((a,b) => a+b, 0)
+    const transferThrottleFinal = transferThrottleArray.reduce((a,b) => a+b, 0)
     return transferThrottleFinal
 }
 
 //Add the transfer bar as soon as the other bars are added to the page
 function transferBarAdd(){
-    let reserveBubble = document.querySelector(".capacity__stats-bubble.reserve");
+    const reserveBubble = document.querySelector(".capacity__stats-bubble.reserve");
     reserveBubble.insertAdjacentHTML('afterend', `<span class="capacity__stats-bubble transfer" style="margin-left:4px; background: #edcd8c; border: solid 1px #ad7301;; color:rgba(0,0,0,0.8);">0 / 0</span>`);
 }
 
 //Add the transfer bar the first time, then add dynamic content on subsequent attempts
 window.setInterval(function(){
-    let transferBubble = document.querySelector(".capacity__stats-bubble.transfer");
+    const transferBubble = document.querySelector(".capacity__stats-bubble.transfer");
     if (transferBubble === null){
         transferBarAdd()
     }else{
-    let transferAvailabilityCaculator = transferThrottleCalculator() - transferLoadCalculator();
+    const transferAvailabilityCaculator = transferThrottleCalculator() - transferLoadCalculator();
     transferBubble.innerHTML = `${transferAvailabilityCaculator} / ${transferThrottleCalculator()}`;
     }
 }, 5000);
-
